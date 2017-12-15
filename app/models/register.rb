82daa3199b9da226e6ac7995f6510da2868cd151
Frankie Roberto <frankie@frankieroberto.com>
2017-12-15 14:36:49 +0000

    destroy dependent records

--- app/models/register.rb
+++ app/models/register.rb
@@ -1,7 +1,7 @@
 class Register < ApplicationRecord
 
-  has_many :entries, foreign_key: 'register_code', primary_key: 'code'
-  has_many :records, foreign_key: 'register_code', primary_key: 'code'
+  has_many :entries, foreign_key: 'register_code', primary_key: 'code', dependent: :destroy
+  has_many :records, foreign_key: 'register_code', primary_key: 'code', dependent: :destroy
 
   def url
     "https://#{code}.#{host || 'register.gov.uk'}"
