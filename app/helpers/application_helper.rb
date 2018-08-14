12197ffbc21daafe93b7215ab3e1b012dc31dbf2
Frankie Roberto <frankie@frankieroberto.com>
2018-08-14 17:27:28 +0100

    WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,25 @@
 module ApplicationHelper
 
+  def linked_named_field(field, key)
+
+    field_record = Record.find_by(register_code: 'field', key: field)
+    associated_register = Register.find_by(code: key)
+
+    if (field_record && field_record.values['register'].present?) || associated_register
+      associated_record = Record.find_by(register_code: field, key: key)
+
+      if associated_record
+        link_to associated_record.name, register_record_path(associated_record.register_code, associated_record.key)
+      else
+        key || "(none)"
+      end
+    else
+      key || "(none)"
+    end
+
+  end
+
+
   def linked_field(field)
 
     if field
