ccf30d907f2a1dd2863a65f9c6e5e2a580bd6474
Frankie Roberto <frankie@frankieroberto.com>
2019-02-13 23:37:51 +0000

    Link to referencing records

--- app/models/record.rb
+++ app/models/record.rb
@@ -11,4 +11,25 @@ class Record < ApplicationRecord
     values.to_h["name"] || values.to_h["hostname"] || values.to_h[register_code] || "???"
   end
 
+  def records_referencing_this_one
+
+    curie_records = Record.where(register_code: 'field').where("values::jsonb @> '{\"datatype\": \"curie\"}'").pluck(:key)
+
+    referencing_records = []
+
+    referencing_records << Record
+      .where.not(register_code: register_code)
+      .where("records.values::jsonb @> '{\"" + register_code + "\" : \"" + key + "\"}'::jsonb")
+
+    curie_records.each do |curie_field|
+
+      referencing_records << Record
+        .where.not(register_code: register_code)
+        .where("records.values::jsonb @> '{\"" + curie_field + "\" : [\"#{register_code}:#{key}\"]}'::jsonb")
+
+    end
+
+    referencing_records.flatten.compact
+  end
+
 end
